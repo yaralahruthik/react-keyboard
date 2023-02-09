544128902c9a34b45a18cafe80442d158f206d16
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-02-09 20:24:37 +0000

    eslint, prettier, git hooks

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+createRoot(document.getElementById('root') as HTMLElement).render(<App />);
